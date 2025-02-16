99cd3a49616e6c297f96651526d720721608ad12
QuentinDV <dassivignonquentin@gmail.com>
2025-02-16 21:30:59 +0100

    weapon maj

---
+++ Assets/Script/Player/Player.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int health = 100;
+    public int maxHealth = 100;
+
+    public int lvl = 1;
+    public int xp = 0;
+
+    public int attack = 10;
+    public int defense = 5;
+
+    public GameObject weapon;
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
